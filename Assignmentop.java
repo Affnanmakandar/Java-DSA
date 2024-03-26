e9da305aa009151afa43d514d8a87e10480e31df
Affnan makandar <123863231+Affnanmakandar@users.noreply.github.com>
2024-03-26 14:16:40 +0530

    Add files via upload

---
+++ Assignmentop.java
@@ -0,0 +1,43 @@
+class JavaBasics{
+    public static void main (String args[]){
+        // 1. '='
+        // int A = 10;
+        //  int B = 5 ;
+        //  System.out.println((A=B)); //output 5 b ki value a mei assign karega 
+        // // 2. '+='
+        // int A = 12;
+        // // we add like this no a = a+ 12 in short we can write
+        // System.out.println((A+=12)); //output 24
+          // 3. '-='
+        // int A = 12;
+        // 
+        // // we add like this no a = a - 12in short we can write
+        // System.out.println((A-=12)); //output 9
+        // 3. '*='
+        // int B = 5;
+        // B*=5;
+        // System.out.println(B); //output 25
+        // or
+        // int B = 5;
+        // int C = 2; 
+        // at normal we wrie c = c*b but in short,
+        // C*=B;
+        // System.out.println(C); //output 10
+         // 4. '/='
+        // int B = 5;
+        //  //B = B /5 kiya
+        //  B /=5;
+        // System.out.println(B); //output 1
+        // or
+        // int B = 2;
+        // int A= 2;
+        // B/=A; //matlab B= B/A kiya
+        // System.out.println(B); // output 1
+        // // we can calculate modulo '%' to calculate remainder
+        // int B = 5;
+        // 
+        // B%=5;
+        // System.out.println(B);//remainder 0
+
+    }
+}
