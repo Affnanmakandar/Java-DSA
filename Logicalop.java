20e2e63159084e7b0e559d336a155a7cfafa12ea
Affnan makandar <123863231+Affnanmakandar@users.noreply.github.com>
2024-03-26 14:17:55 +0530

    Add files via upload

---
+++ Logicalop.java
@@ -0,0 +1,18 @@
+class JavaBasics{
+    public static void main(String args[]){
+        // Logical and '&&'
+        // System.out.println((3>2) && (5>0)); true
+        // Logical or  or '||'
+        // System.out.println((3<2) || (5<0)); false aur false = false
+        // or
+        // System.out.println((3>2) || (5<0)); true aur false = true
+        // or
+        // System.out.println((3<2) || (5>0)); //false aur true = true
+        // or
+        // System.out.println((3>2) || (5>0)); //true aur true = true
+        // Not '!' or logical not
+        // System.out.println(!(3>2)); //true ko false banaega
+        // or else 
+        // System.out.println(!(0>5)); //false ko true banaega
+    }
+}
---
+++ Relationalop.java
@@ -0,0 +1,27 @@
+class JavaBasics{
+    public static void main(String args[]){
+        // int a = 10;
+        // int b = 10;
+        // System.out.println(a==b); if this is equal then it is true
+        // not equal to operator '!='
+        // int a = 10;
+        // int b = 10;
+        // System.out.println(a!= b); false because it defines not equal to.
+        // greater than '>'
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a>b); //true
+        // less than '<'
+        // int a = 20;
+        // int b = 50;
+        // System.out.println(a<b); //true
+        // greater than equal to '>='
+        // int a = 10;
+        // int b = 10;
+        // System.out.println(a>=b); //true because it is  greater than or equal  to b
+        // less than equal to '<='
+        // int a = 5;
+        // int b = 5;
+        // System.out.println(a<=b);
+    }
+}
