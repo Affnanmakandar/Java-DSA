1aa036aa89a3dd8dc3fc48802aa77ea150d2ea38
Affnan makandar <123863231+Affnanmakandar@users.noreply.github.com>
2024-03-26 14:18:49 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,20 @@
+
+class JavaBasics{
+    public static void main(String args[]){
+    // example on code
+    // int a = 10;
+    //  int b = ++a; //increased by 1
+    //  System.out.println(a);
+    //  System.out.println(b);
+    //  int a = 10;
+    //  int b = a++; //increased by 1
+    //  System.out.println(a);
+    //  System.out.println(b);
+     int a = 10;
+     int b = a--; //increased by 1
+     System.out.println(a);
+     System.out.println(b);
+    
+    }
+   
+}
